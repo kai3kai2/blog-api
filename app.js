7e3a8aa62111d2fe04bdf090eebdc959097610d2
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2023-04-26 22:08:12 +0800

    feat: project init

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const expressHandlebars = require('express-handlebars')
+const methodOverride = require('method-override')
+const bcrypt = require('bcryptjs')
 const app = express()
 const port = 3000
 
+app.engine('hbs', expressHandlebars({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.get('/', (req, res) => res.send('Twitter'))
 
 app.listen(port, () => console.log(`Smiple Twitter is listening on port ${port}`))
