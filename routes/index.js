868424c553ffdae9920d47c68b74bfc634ff4d5e
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2023-05-17 23:19:55 +0800

    feat: add register page

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const blogController = require('../controllers/blog-controller')
 const admin = require('./modules/admin')
+const blogController = require('../controllers/blog-controller')
+const userController = require('../controllers/user-controller')
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/blog', blogController.getBlog)
-router.use('/', (req, res) => res.render('/restaurants'))
+router.use('/', (req, res) => res.render('blog'))
 
 module.exports = router
