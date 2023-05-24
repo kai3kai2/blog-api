03091ec8a4a236b0bc14de9fd679692303633a50
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2023-05-24 20:33:37 +0800

    add header & footer

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const expressHandlebars = require('express-handlebars')
 const session = require('express-session')
 const flash = require('connect-flash')
 const passport = require('./config/passport')
+const { getUser } = require('./helpers/auth-helpers')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -12,7 +14,7 @@ if (process.env.NODE_ENV !== 'production') {
 const app = express()
 const port = 3000
 
-app.engine('hbs', expressHandlebars({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', expressHandlebars({ defaultLayout: 'main', extname: '.hbs', helpers: handlebarsHelpers })) // 將 handlebars-helpers 註冊進express-handlebar
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }))
@@ -23,6 +25,7 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
+  req.locals = getUser
   next()
 })
 app.use('/api', apis)
